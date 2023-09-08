018e2c1e1c1d5e5263cf3cf300a10cb93d5eea5f
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-09 03:19:51 +0530

    chore: tailwind setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
-function App() {
-
+export default function App() {
   return (
-    <>
-        <h1>Test</h1>  
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
---
+++ client/src/view/app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function index({}: Props) {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
