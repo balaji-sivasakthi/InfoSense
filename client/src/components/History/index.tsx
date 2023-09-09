6b5aa1ecbafa43d21950a4d33b10f1d2b1ce878e
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-09 09:37:43 +0530

    feat: history component

--- client/src/components/History/index.tsx
+++ client/src/components/History/index.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import Button from "../Button";
 
 type Props = {
- data:{
-  id:string;
-  title:string;
-  category:string;
- }
-}
+  data: {
+    id: string;
+    title: string;
+    category: string;
+    url: string;
+  };
+};
 
-function History({data}: Props) {
-  const {id, title, category} = data
+function History({ data }: Props) {
+  const { id, title, category, url } = data;
+  const navigate = useNavigate();  
+  
   return (
-    <div className='border-2 p-3 mb-2 rounded-sm'>
-      <h1 className='text-2xl'>{title}</h1>
-      <p className='bg-blue-300 p-2 w-28 text-center'>{category}</p>
+    <div className="border-2 p-3 mb-2 rounded flex justify-between items-center">
+      <div className="space-y-4">
+        <h1 className="text-2xl mb-2">{title}</h1>
+        <span className="bg-blue-300 rounded-lg px-2 py-1">{category}</span>
+        <p className="text-gray-500">{url}</p>
+      </div>  
+      <div>
+        <Button onClick={()=>navigate(`/app/details/${id}`)}>View</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default History
+export default History;
