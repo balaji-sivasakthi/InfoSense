98fd2f84d038c2294cc6012fb9b74044bf432cf1
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-09 11:47:58 +0530

    feat: written trancate utils

--- client/src/utils/index.tsx
+++ client/src/utils/index.tsx
@@ -4,3 +4,7 @@ import { clsx } from "clsx";
 export function cn(...inputs:any[]) {
   return twMerge(clsx(inputs));
 }
+
+export function truncate(str:string, n:number) {
+  return str.length > n ? str.slice(0, n - 1) + "..." : str;
+}
