7c4a639157798404bd1fa1fc2d60a0725c73304e
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-09 09:35:17 +0530

    feat: recent history

--- client/src/view/app/index.tsx
+++ client/src/view/app/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import AppLayout from '../../layouts/AppLayout'
+import History from '../../components/History'
 
 type Props = {}
 
 function index({}: Props) {
+  const recentHistory = [
+    {
+      id:"1",
+      title:"Hello",
+      category:"tech",
+      url:"https://www.example.com"
+    },
+    {
+      id:"2",
+      title:"Hello",
+      category:"tech",
+      url:"https://www.example.com"
+    }
+  ]
   return (
-    <div>index</div>
+    <AppLayout>
+        {recentHistory.map(item=>{
+          return <History key={item.id} data={item}/>
+        })}
+    </AppLayout>
   )
 }
 
