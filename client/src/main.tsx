68b86e6ee3da746178f31d8a4b7f0f413270f6fa
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-09 07:49:24 +0530

    feat: added two routes home and app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './view/app'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './view/app/Home'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' Component={HomePage}/>
+            <Route path='/app/dashboard' Component={App}/>
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
