428c24c3ebdbf36a87528ee781fc3e7809070967
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-09 09:38:18 +0530

    refactor: applayout based on conditional rendering

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -1,18 +1,20 @@
 import React, { ReactElement } from 'react'
 import SideBar from '../components/Sidebar'
 import TopBar from '../components/TopBar'
+import { useLocation } from 'react-router-dom'
 
 type Props = {
   children:React.ReactNode
 }
 
 function AppLayout({children}: Props) {
+  const {pathname}= useLocation()
   return (
     <div className='flex'>
       <SideBar/>
         <main className='flex-1 p-3'>
-          <TopBar />
-          <div className='min-h-screen  rounded-lg border-2 p-3'>
+        {pathname=="/app/dashboard" && <TopBar />}
+          <div className='min-h-screen rounded-lg border-2 p-3'>
             {children}
           </div>
         </main>
