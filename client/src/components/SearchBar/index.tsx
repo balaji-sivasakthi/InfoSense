25210bd3e2dbf37fa14e9106f2ac2cb4400a98ad
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-11 00:28:23 +0530

    feat: regex pattern match

--- client/src/components/SearchBar/index.tsx
+++ client/src/components/SearchBar/index.tsx
@@ -14,8 +14,12 @@ function SearchBar({}: Props) {
   const dispatch = useDispatch<AppDispatch>()
 
   const handleOnPredict  = async ()=>{
-
-    if(news_url!=""){
+    let url_pattern = "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
+    let regex = new RegExp(url_pattern)
+    if(!news_url.match(regex)){
+      alert("Invaild Url")  
+      return;
+    }
         dispatch(changeLoadingState(true))
       await ApiFactory
         .create(ApiFactory.history)
@@ -25,8 +29,6 @@ function SearchBar({}: Props) {
 
       dispatch(changeLoadingState(false))
       setUrl("")
-    }
-    return;
   }
 
   return (
@@ -34,6 +36,7 @@ function SearchBar({}: Props) {
         <input 
           className='w-11/12 border-2 focus:outline-none p-2 rounded-lg'
           type="text"
+          value={news_url}
           onChange={(event)=> setUrl(event.target.value)} 
           placeholder='Enter the Url' />
         <Button onClick={handleOnPredict} className="w-1/12">Predict</Button>
