7038c2a05db53e0e8e56ec1ab9d8dc7c94d1f53a
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-11 00:27:42 +0530

    feat: add link to the history card

--- client/src/components/History/index.tsx
+++ client/src/components/History/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../Button";
 import { truncate } from "../../utils";
 
@@ -16,11 +16,13 @@ function History({ data }: Props) {
   const navigate = useNavigate();  
   
   return (
-    <div className="border-2 p-3 mb-2 rounded flex justify-between items-center">
+    <div className="border-2 p-3 mb-2 rounded flex justify-between items-center hover:cursor-pointer hover:border-blue-600">
       <div className="space-y-4">
         <h1 className="text-2xl mb-2">{truncate(title,70)}</h1>
         <span className="bg-blue-300 rounded-lg px-2 py-1">{category}</span>
-        <p className="text-gray-500">{news_url}</p>
+        <p className="text-gray-500">
+            <Link to={news_url}>{news_url}</Link>
+          </p>
       </div>  
       <div>
         <Button onClick={()=>navigate(`/app/details/${id}`)}>View</Button>
