9fb83b456a3054d926ea8ec32d024cf52f4d157a
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-11 11:25:01 +0530

    fix: base url path

--- client/src/api/http-common.ts
+++ client/src/api/http-common.ts
@@ -4,8 +4,8 @@ import axios from "axios";
 const APP_MODE = import.meta.env.VITE_APP_MODE
 
 let base_url = (APP_MODE == 'development') 
-? import.meta.env.VITE_APP_BASE_URL
-: import.meta.env.VITE_APP_DEV_BASE_URL
+? import.meta.env.VITE_APP_DEV_BASE_URL
+: import.meta.env.VITE_APP_BASE_URL
 
 let intance:Axios = axios.create({
     baseURL: base_url,
