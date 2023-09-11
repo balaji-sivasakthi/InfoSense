f1a1d905c948d129ffe37540acd3b0bcfc77fb08
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-11 12:43:32 +0530

    fix: try now button on landing page

--- client/src/containers/Hero/index.tsx
+++ client/src/containers/Hero/index.tsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 
 type Props = {};
 
 function Hero({}: Props) {
+  const navigate = useNavigate()
   return (
     <div className="flex flex-col h-screen justify-center items-center space-y-4 px-[250px]">
       <span className="text-xl text-blue-700 text-center">Meet InfoSense</span>
@@ -13,7 +15,7 @@ function Hero({}: Props) {
         Revolutionize your content creation with advanced AI. Create exceptional
         content quickly and easily, and gain a leading edge in your industry.
       </p>
-      <Button onClick={()=>{}}>Try For Free</Button>
+      <Button onClick={()=>navigate('/app/dashboard')}>Try For Free</Button>
     </div>
   );
 }
