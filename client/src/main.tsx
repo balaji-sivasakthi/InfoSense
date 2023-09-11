18e51e8fde198133ffcc205c9fbb927e4e304eb6
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-11 12:55:38 +0530

    chore: clear console.log if it is production

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux";
 import { Provider } from "react-redux";
 
+if(import.meta.env.VITE_APP_MODE != "development"){
+  window.console.log = ()=>{}
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <React.StrictMode>
