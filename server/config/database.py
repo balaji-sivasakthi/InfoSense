52f8612257feefad90a35608077a1be176c43ff3
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-11 09:55:50 +0530

    chore: url from env

--- server/config/database.py
+++ server/config/database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv() 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:readme@localhost:5432/info-sense"
+SQLALCHEMY_DATABASE_URL = os.environ.get('DB_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 session = SessionLocal()
 Base = declarative_base()
-    
+
